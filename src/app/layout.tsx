fa36ded3abadb5acac26297489063adc6c37659f
Stullet <lirabusiness7@gmail.com>
2026-07-15 02:44:12 +0000

    PRECISO QUE ADICIONE TAMBÉM O MEU CÓDIGO DO PIXEL NO HEAD DA PÁGINNA:
    <

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,18 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Utmify Global Pixel */}
+        <Script id="utmify-global-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "6a56f3ac3ac58c2a46b2a9bc";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+
         {/* Utmify UTM Tracking */}
         <Script
           id="utmify-tracking"
